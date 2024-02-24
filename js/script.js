b2decd10a4829f060376d092d445221dbb581911
RUAN DOS SANTOS <100040124+Ruandossantos@users.noreply.github.com>
2024-02-23 21:40:02 -0300

    4.1%

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-const titulo = document.getElementsByName('h1');
-console.log(titulo);
+//FUNÇÃO QUE FAZ EFEITO DE DIGITAÇÃO NO H1 HEADER
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 95 * i)
+    );
+
+    setInterval(() => typeWriter(el), 12000);
+}
+typeWriter(letra);
